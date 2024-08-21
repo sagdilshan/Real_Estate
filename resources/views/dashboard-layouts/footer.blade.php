5cd72456ff7d37ee956455f0385b7ca8ebf07fb7
Gayathra Dilshan <gayathradilshan1@gmail.com>
2024-08-21 12:01:13 +0530

    Update footer.blade.php

--- resources/views/dashboard-layouts/footer.blade.php
+++ resources/views/dashboard-layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; 2024 | <a target="_blank" href="https://lk.linkedin.com/in/gayathra-dilshan-347775238" style="color: red">Woodify - Crafting Dreams into Wooden Reality</a> |
+    <strong>Copyright &copy; 2024 | <a target="_blank" href="https://lk.linkedin.com/in/gayathra-dilshan-347775238" style="color: red">Real Estate</a> |
         All Rights Reserved. </strong>
     <div class="float-right d-none d-sm-inline-block">
-      Developed By <b>Gayathra Dilshan</b>
+      Developed By <b>Elysian Techlabs</b>
     </div>
   </footer>
