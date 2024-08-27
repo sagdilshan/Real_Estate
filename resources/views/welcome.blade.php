f082f76b481a65915f21dee7c056d43e521c2dab
Gayathra Dilshan <gayathradilshan1@gmail.com>
2024-08-27 19:03:32 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -863,11 +863,11 @@
                     </nav>
 
                     @elseif(Auth::user()->role == 'manager')
-                        <li><a href="{{ route('admin.logout') }}"
+                        <li><a href=""
                                 class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Logout</a>
                         </li>
                     @elseif(Auth::user()->role == 'guest')
-                        <li><a href="{{ route('admin.logout') }}"
+                        <li><a href=""
                                 class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Logout</a>
                         </li>
                     @endif
@@ -887,7 +887,7 @@
         <div class="max-w-7xl mx-auto p-6 lg:p-8">
 
 
-           
+
         </div>
     </div>
 </body>
