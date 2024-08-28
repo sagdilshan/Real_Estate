5af80fc4c608b53010ba72126fa4c3e41444cd73
Gayathra Dilshan <gayathradilshan1@gmail.com>
2024-08-28 18:13:30 +0530

    Update footer.blade.php

--- resources/views/dashboard-layouts/footer.blade.php
+++ resources/views/dashboard-layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; 2024 | <a target="_blank" href="https://lk.linkedin.com/in/gayathra-dilshan-347775238" style="color: red">Real Estate</a> |
+    <strong>Copyright &copy; 2024 | <a target="_blank" href=" " style="color: red">Real Estate</a> |
         All Rights Reserved. </strong>
     <div class="float-right d-none d-sm-inline-block">
       Developed By <b> <a style="color: rgb(255, 0, 0)" href="https://elysiantechlabs.com/">Elysian Techlabs</a></b>
